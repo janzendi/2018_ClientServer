a3bd0692952cdc8f39ffb7f245f21c7d7418dfac
Dimi <dim.janzen@gmail.com>
2018-09-22 15:56:18 +0200

    An OPC Interface weitergearbeitet.

---
+++ Client/global/objects/twizard/OPCTag.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Client.global.objects.twizard
+{
+    class OPCTag
+    {
+        #region Konstruktoren
+        /// <summary>
+        /// Standardkonstruktor
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public OPCTag()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="variablename"></param>
+        /// <created>janzen_d,2018-09-22</created>
+        public OPCTag(string variablename)
+        {
+            this.strvariablename = variablename;
+        }
+        #endregion
+
+        #region Variablen mit Setter Eigenschalften
+        private int listid;
+        /// <summary>
+        /// List ID
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public int LISTID
+        {
+            set
+            {
+                this.listid = value;
+            }
+        }
+
+        private string strmodulename;
+        /// <summary>
+        /// Modulname setzen. Es muss der komplette Pfad übergeben werden und es wird der letzte Ordner als Modulname benutzt
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public object MODULENAME
+        {
+            set
+            {
+                string[] arrPathSplit = value.ToString().Split(new Char[] { '\\' });
+                this.strmodulename = arrPathSplit[arrPathSplit.Length - 1];
+            }
+        }
+
+        private string strvariablename;
+        /// <summary>
+        /// Variablenname aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string VARIABLENAME
+        {
+            set
+            {
+                this.strvariablename = value;
+            }
+        }
+
+        private string strcmt;
+        /// <summary>
+        /// Kommentar aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string COMMENT
+        {
+            set
+            {
+                this.strcmt = value;
+            }
+        }
+
+        private int inttextid;
+        /// <summary>
+        /// TextID aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string TEXTID
+        {
+            set
+            {
+                if (System.Int32.TryParse(value, out int result))
+                    this.inttextid = result;
+                else
+                    this.inttextid = 0;
+            }
+        }
+
+        private bool opcactive;
+        /// <summary>
+        /// OPC aktiviert aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string OPCACTIVE
+        {
+            set
+            {
+                switch (value)
+                {
+                    case "0":
+                        this.opcactive = false;
+                        break;
+                    case "1":
+                        this.opcactive = true;
+                        break;
+                    default:
+                        this.opcactive = false;
+                        break;
+                }
+            }
+        }
+
+        private string stropcDataType;
+        /// <summary>
+        /// OPC Datentyp aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string OPCDATATYPE
+        {
+            set
+            {
+                this.stropcDataType = value;
+            }
+        }
+
+        private int intopcrwu;
+        /// <summary>
+        /// Schreib Leserechte für OPC Server in pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string OPCRWU
+        {
+            set
+            {
+                if (System.Int32.TryParse(value, out int result))
+                    this.intopcrwu = result;
+                else
+                    this.intopcrwu = 0;
+            }
+        }
+
+        private string stropcalias;
+        /// <summary>
+        /// OPC alias in pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string OPCALIAS
+        {
+            set
+            {
+                this.stropcalias = value;
+            }
+        }
+
+        private string strsemantic;
+        /// <summary>
+        /// OPC semantic in pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string SEMANTIC
+        {
+            set
+            {
+                this.stropcalias = value;
+            }
+        }
+
+        private string strdcreadperiod;
+        /// <summary>
+        /// datacollector read period in pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string DCREADPERIOD
+        {
+            set
+            {
+                this.strdcreadperiod = value;
+            }
+        }
+
+        private string strlogthreshold;
+        /// <summary>
+        /// datacollector log threshold in pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string LOGTHRESHOLD
+        {
+            set
+            {
+                this.strlogthreshold = value;
+            }
+        }
+
+        private string strdcmaxpersistence;
+        /// <summary>
+        /// Data Collector max persistence in pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string DCMAXPERSISTENCE
+        {
+            set
+            {
+                this.strdcmaxpersistence = value;
+            }
+        }
+
+        private string strpvprocesspath;
+        /// <summary>
+        /// pv_process Pfad
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string PVPROCESSPATH
+        {
+            set
+            {
+                this.strpvprocesspath = value;
+            }
+        }
+
+        private int intpdacountertype;
+        /// <summary>
+        /// BDE Counter Typ aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string PDACOUNTERTYPE
+        {
+            set
+            {
+                if (System.Int32.TryParse(value, out int result))
+                    this.intpdacountertype = result;
+                else
+                    this.intpdacountertype = 0;
+            }
+        }
+
+        private int intarrysize;
+        /// <summary>
+        /// BDE Counter Typ aus pv_process
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string ARRAYSIZE
+        {
+            set
+            {
+                if (System.Int32.TryParse(value, out int result))
+                    this.intarrysize = result;
+                else
+                    this.intarrysize = 1;
+            }
+        }
+
+        private string strpvequipmentpath;
+        /// <summary>
+        /// pv_process Pfad
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string PVEQUIPMENTPATH
+        {
+            set
+            {
+                this.strpvequipmentpath = value;
+            }
+        }
+        #endregion
+
+        #region Rückgabe Methoden und Eigenschaften
+        /// <summary>
+        /// Gibt eine Zeile zurück.
+        /// </summary>
+        /// <created>janzen_d,2018-09-22</created>
+        public string[] GetGridRow
+        {
+            get
+            {
+                try
+                {
+                    return new string[] { listid.ToString(), strmodulename };
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Überschreibt die Equals Methode damit List<>.contains anhand von dem Variablennamen funktioniert.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>true = falls Variablennamen übereinstimmt</returns>
+        /// <created>janzen_d,2018-09-22</created>
+        public override bool Equals(object obj)
+        {
+            if (obj is OPCTag)
+            {
+                if (((OPCTag)obj).strvariablename == this.strvariablename)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Wird benötigt, da Equals überschrieben wird.
+        /// </summary>
+        /// <returns></returns>
+        /// <created>janzen_d,2018-09-22</created>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        #endregion
+    }
+}
--- Client/panel/TW_STOPC.cs
+++ Client/panel/TW_STOPC.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using MetroFramework.Forms;
 using MetroFramework.Controls;
-using MetroFramework.Fonts;
+using Client.global.objects.twizard;
+using System.IO;
+using System.Threading;
+using System.Xml;
 
 namespace Client.panel
 {
@@ -18,10 +15,264 @@ namespace Client.panel
     /// <created>janzen_d,2018-09-21</created>
     public partial class TW_STOPC : MetroUserControl
     {
+        private List<OPCTag> listOpcTags;
+        private List<Thread> listThreads;
+
         public TW_STOPC(MetroFramework.Components.MetroStyleManager metroStyleManager)
         {
             InitializeComponent();
             this.CustomInitializeComponent(metroStyleManager); // deklariert im *.Designer.cs
+
+            listOpcTags = new List<OPCTag>();
+            listThreads = new List<Thread>();
+        }
+
+        public void Close()
+        {
+            global.config.ConfigReadWriter.TWOPCPATH=this.mtxtBox_TWPath_1038.Text;
+        }
+
+        /// <summary>
+        /// Event um Twizard einzulesen.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <created>janzen_d,2018-09-21</created>
+        private void MbtnSelectFolder_1039_Click(object sender, System.EventArgs e)
+        {
+            global.log.MetroLog.INSTANCE.WriteLine("", global.log.MetroLog.LogType.INFO, 1041);
+            folderBrowserDialog.SelectedPath = mtxtBox_TWPath_1038.Text; // versuchen Ordner aus der Textbox setzen
+            if (folderBrowserDialog.ShowDialog(this) == DialogResult.OK) // Ordner öffnen
+            {
+                if (Directory.Exists(folderBrowserDialog.SelectedPath + "\\platform\\fm") && File.Exists(folderBrowserDialog.SelectedPath + "\\build\\version_1\\process.xml"))
+                {
+                    //Alte Daten löschen
+                    metroGrid.Rows.Clear();
+                    listOpcTags.Clear();
+                    listThreads.Clear();
+
+                    //
+                    // Ordner gefunden
+                    //
+                    mtxtBox_TWPath_1038.Text = folderBrowserDialog.SelectedPath;
+                    global.config.ConfigReadWriter.TWADPREPATH = folderBrowserDialog.SelectedPath;
+                    global.log.MetroLog.INSTANCE.WriteLine("", global.log.MetroLog.LogType.INFO, 1053);
+
+                    //
+                    // Temp Liste erstellen
+                    //
+                    List<string> activemodules = new List<string>();
+
+                    // \\build\\version_1\\process.xml durchsuchen auf aktive Module
+                    System.Xml.XmlDocument xmlDocumentProcessXML = new System.Xml.XmlDocument();
+                    xmlDocumentProcessXML.XmlResolver = null;
+                    xmlDocumentProcessXML.Load(folderBrowserDialog.SelectedPath + "\\build\\version_1\\process.xml");
+                    foreach (System.Xml.XmlNode fmNode in xmlDocumentProcessXML.SelectNodes("/PROCESS/PM/FLOW/FM"))
+                    {
+                        try
+                        {
+                            if (fmNode.Attributes["act"].Value == "1" && fmNode.Attributes["dir"].Value != null && fmNode.Attributes["dir"].Value != System.String.Empty && fmNode.Attributes["dir"].Value != "")
+                                activemodules.Add(fmNode.Attributes["dir"].Value);
+                        }
+                        catch (Exception)
+                        {
+                            continue;
+                        }
+                    }
+                    global.log.MetroLog.INSTANCE.WriteLine(" " + activemodules.Count.ToString(), global.log.MetroLog.LogType.INFO, 1054);
+
+                    //
+                    // Alle euqipment Dateien durchsuchen
+                    //
+                    DirectoryInfo dcPlattform = new DirectoryInfo(folderBrowserDialog.SelectedPath + "\\platform\\fm");
+                    int directoryCount = dcPlattform.GetDirectories().Length;
+                    int i = 0;
+                    foreach (DirectoryInfo directory in dcPlattform.GetDirectories())
+                    {
+                        if (activemodules.Contains(directory.Name))
+                        {
+                            // Log Informationen
+                            global.log.MetroLog.INSTANCE.DebugWriteLine(" " + directory.FullName, global.log.MetroLog.LogType.INFO, 1055);
+
+                            //
+                            // Für jedes equipmentfile ein Thread starten
+                            //
+                            if (Directory.Exists(directory.FullName + "\\uhlmann\\tc\\pv_equipment") && Directory.Exists(directory.FullName + "\\uhlmann\\tc\\pv_process"))
+                            {
+                                // Thread starten
+                                Thread thread = new Thread(new ParameterizedThreadStart(ThreadAnalyzeTWOPC));
+                                listThreads.Add(thread);
+                                thread.Start(directory.FullName);
+                            }
+                        }
+                        global.log.MetroLog.INSTANCE.ProgressBar((decimal)(++i) / (decimal)directoryCount, directory.Name);
+                    }
+
+                    //
+                    // prüfen ob alle Threads fertig sind
+                    //
+                    bool finishloop = false;
+                    while (finishloop)
+                    {
+                        int finishcount = 0;
+                        for (int icount = 0; i < listThreads.Count; i++)
+                        {
+                            if (!listThreads[icount].IsAlive)
+                                finishcount++;
+                        }
+                        if (finishcount == (listThreads.Count - 1))
+                            finishloop = true;
+                    }
+                    // Save button aktivieren
+                    mbtnSave_1040.Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Thread um Twizard analisieren
+        /// </summary>
+        /// <param name="twizardpath"></param>
+        /// <created>janzen_d,2018-09-21</created>
+        private void ThreadAnalyzeTWOPC(object twizardpath)
+        {
+            if (twizardpath != null && twizardpath.ToString().Length > 3)
+            {
+                //
+                // analyze pv_process
+                //
+                if (Directory.Exists(twizardpath.ToString() + "\\uhlmann\\tc\\pv_process"))
+                {
+                    string[] paths_pv_process = Directory.GetFiles(twizardpath.ToString() + "\\uhlmann\\tc\\pv_process");
+                    foreach (string item in paths_pv_process)
+                    {
+                        string extension = Path.GetExtension(item);
+                        if (extension == ".xml" || extension == ".XML")
+                        {
+                            XmlDocument xmlDocument = new XmlDocument();
+                            xmlDocument.XmlResolver = null;
+                            xmlDocument.Load(item);
+                            foreach (XmlNode xmlNode in xmlDocument.SelectNodes("PPVS/PPV[@name]"))
+                            {
+                                try
+                                {
+                                    OPCTag oPCTag = new OPCTag(xmlNode.Attributes["name"].Value);
+                                    if (!listOpcTags.Contains(oPCTag))
+                                    {
+                                        listOpcTags.Add(oPCTag);
+                                        oPCTag.LISTID = listOpcTags.IndexOf(oPCTag);
+                                        oPCTag.MODULENAME = twizardpath.ToString();
+                                        oPCTag.PVPROCESSPATH = item;
+                                        foreach (XmlAttribute xmlAttribute in xmlNode.Attributes)
+                                        {
+                                            switch (xmlAttribute.Name)
+                                            {
+                                                case "varTxt":  // Text-ID
+                                                    oPCTag.TEXTID = xmlAttribute.Value;
+                                                    break;
+                                                case "cmt":     // Kommentar
+                                                    oPCTag.COMMENT = xmlAttribute.Value;
+                                                    break;
+                                                case "opcActive":
+                                                    oPCTag.OPCACTIVE = xmlAttribute.Value;
+                                                    break;
+                                                case "opcDataType":
+                                                    oPCTag.OPCDATATYPE = xmlAttribute.Value;
+                                                    break;
+                                                case "opcRwu":
+                                                    oPCTag.OPCRWU = xmlAttribute.Value;
+                                                    break;
+                                                case "opcAlias":
+                                                    oPCTag.OPCALIAS = xmlAttribute.Value;
+                                                    break;
+                                                case "semantic":
+                                                    oPCTag.SEMANTIC = xmlAttribute.Value;
+                                                    break;
+                                                case "dcReadPeriod":
+                                                    oPCTag.DCREADPERIOD = xmlAttribute.Value;
+                                                    break;
+                                                case "dcLogThreshold":
+                                                    oPCTag.LOGTHRESHOLD = xmlAttribute.Value;
+                                                    break;
+                                                case "dcMaxPersistence":
+                                                    oPCTag.DCMAXPERSISTENCE = xmlAttribute.Value;
+                                                    break;
+                                                case "pdaCounterType":
+                                                    oPCTag.PDACOUNTERTYPE = xmlAttribute.Value;
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                        }
+                                    }
+                                }
+                                catch (Exception)
+                                {
+                                    continue;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                //
+                // pv_equipment analysieren
+                //
+                if (Directory.Exists(twizardpath.ToString() + "\\uhlmann\\tc\\pv_equipment"))
+                {
+
+                    string[] paths_pv_equipment = Directory.GetFiles(twizardpath.ToString() + "\\uhlmann\\tc\\pv_process");
+                    foreach (string path in paths_pv_equipment)
+                    {
+                        string extension = Path.GetExtension(path);
+                        if (extension == ".xml" || extension == ".XML")
+                        {
+                            XmlDocument xmlDocument = new XmlDocument();
+                            xmlDocument.XmlResolver = null;
+                            xmlDocument.Load(path);
+                            foreach (XmlNode xmlNode in xmlDocument.SelectNodes("EPVS/EPV[@name]"))
+                            {
+                                try
+                                {
+                                    OPCTag oPCTag = new OPCTag(xmlNode.Attributes["name"].Value);
+                                    if (listOpcTags.Contains(oPCTag))
+                                    {
+                                        int index = listOpcTags.IndexOf(oPCTag);
+                                        foreach (XmlAttribute xmlAttribute in xmlNode.Attributes)
+                                        {
+                                            switch (xmlAttribute.Name)
+                                            {
+                                                case "size":
+                                                    listOpcTags[index].ARRAYSIZE = xmlAttribute.Value;
+                                                    listOpcTags[index].PVEQUIPMENTPATH = path;
+                                                    break;
+                                                default:
+                                                    break;
+                                            }
+                                        }
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    throw;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event um Änderungen zu speichern
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <created>janzen_d,2018-09-21</created>
+        private void MbtnSave_1040_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
